a60330e3f63f12577a9c26bc159eacacd66c7e3b
Ignat Mora <morrig407@gmail.com>
2022-02-14 15:30:58 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Hello from Main()");
+        HW1.QueueTime(new int[] { 10, 2, 3, 3 }, 2);
+	}
+}
+
+public class HW1
+{
+	public static long QueueTime(int[] customers, int n)
+	{
+		int nextPerson = 0;
+		int count = 0;
+		int[] que = new int[n];
+		int step = 0;
+		while (true)
+        {
+			if (count < customers.Length)
+			{
+				int emptyQue = getEmptyQue(que);
+				if (emptyQue != -1)
+				{
+					que[emptyQue] = customers[nextPerson];
+					nextPerson++;
+					count++;
+				}
+			}
+
+			makeStep(que, ref step);
+
+			if (count >= customers.Length && isQueEmpty(que))
+				break;
+		}
+		Console.WriteLine($"Steps: {step}");
+		return step;
+
+		int getEmptyQue(int[] que)
+        {
+			for (int i = 0; i < que.Length; ++i)
+            {
+				if (que[i] == 0)
+					return i;
+            }
+			return -1;
+        }
+
+		void makeStep(int[] que, ref int step)
+        {
+			for (int i = 0; i < que.Length; ++i)
+            {
+				if (que[i] > 0)
+					--que[i];
+            }
+			++step;
+        }
+
+		bool isQueEmpty(int[] que)
+        {
+			foreach (var el in que)
+            {
+				if (el != 0)
+					return false;
+            }
+			return true;
+        }
+	}
+}
