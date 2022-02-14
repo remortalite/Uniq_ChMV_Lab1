8f058bfa90ebb4e97a48cd4cdfdfdbe9a76f4b68
Ignat Mora <morrig407@gmail.com>
2022-02-14 16:45:33 +0300

    Add tests

--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@
 	public static void Main(string[] args)
 	{
 		Console.WriteLine("Hello from Main()");
-        HW1.QueueTime(new int[] { 10, 2, 3, 3 }, 2);
-	}
+
+		TestHW1.Run();
+    }
 }
 
 public class HW1
@@ -15,17 +16,16 @@ public class HW1
 		int count = 0;
 		int[] que = new int[n];
 		int step = 0;
+		if (customers.Length == 0)
+			return 0;
 		while (true)
         {
-			if (count < customers.Length)
+			int emptyQue;
+			while ((emptyQue = getEmptyQue(que)) != -1 && count < customers.Length)
 			{
-				int emptyQue = getEmptyQue(que);
-				if (emptyQue != -1)
-				{
-					que[emptyQue] = customers[nextPerson];
-					nextPerson++;
-					count++;
-				}
+				que[emptyQue] = customers[nextPerson];
+				nextPerson++;
+				count++;
 			}
 
 			makeStep(que, ref step);
@@ -33,7 +33,7 @@ public class HW1
 			if (count >= customers.Length && isQueEmpty(que))
 				break;
 		}
-		Console.WriteLine($"Steps: {step}");
+		//Console.WriteLine($"Steps: {step}");
 		return step;
 
 		int getEmptyQue(int[] que)
@@ -66,4 +66,169 @@ public class HW1
 			return true;
         }
 	}
+}
+
+public class TestHW1
+{
+
+	public static void Run()
+    {
+		Test1();
+		Test2();
+		Test3();
+		Test4();
+		Test5();
+		Test6();
+		Test7();
+		Test8();
+
+		PrintResults();
+    }
+
+	static int Test1()
+	{
+		string name = "Test1";
+		int[] customers = { 5, 3, 4 };
+		int n = 1;
+		long result = HW1.QueueTime(customers, n);
+		long resultCorrect = 12;
+		if (result == resultCorrect)
+		{
+			testSuccess(name);
+			return 1;
+		}
+		testFail(name);
+		return 0;
+	}
+
+	static int Test2()
+	{
+		string name = "Test2";
+		int[] customers = { 10, 2, 3, 3 };
+		int n = 2;
+		long result = HW1.QueueTime(customers, n);
+		long resultCorrect = 10;
+		if (result == resultCorrect)
+		{
+			testSuccess(name);
+			return 1;
+		}
+		testFail(name);
+		return 0;
+	}
+
+	static int Test3()
+	{
+		string name = "Test3";
+		int[] customers = { 2, 3, 10 };
+		int n = 2;
+		long result = HW1.QueueTime(customers, n);
+		long resultCorrect = 12;
+		if (result == resultCorrect)
+		{
+			testSuccess(name);
+			return 1;
+		}
+		testFail(name);
+		return 0;
+	}
+
+	static int Test4()
+	{
+		string name = "Test4";
+		int[] customers = { 5, 4, 3, 4, 5 };
+		int n = 3;
+		long result = HW1.QueueTime(customers, n);
+		long resultCorrect = 9;
+		if (result == resultCorrect)
+		{
+			testSuccess(name);
+			return 1;
+		}
+		testFail(name);
+		return 0;
+	}
+
+	static int Test5()
+	{
+		string name = "Test5";
+		int[] customers = { 5, 4, 3, 4, 5, 4, 2 };
+		int n = 3;
+		long result = HW1.QueueTime(customers, n);
+		long resultCorrect = 9;
+		if (result == resultCorrect)
+		{
+			testSuccess(name);
+			return 1;
+		}
+		testFail(name);
+		return 0;
+	}
+
+	static int Test6()
+	{
+		string name = "Test6";
+		int[] customers = { 5, 4, 3, 4, 5, 4, 3 };
+		int n = 3;
+		long result = HW1.QueueTime(customers, n);
+		long resultCorrect = 10;
+		if (result == resultCorrect)
+		{
+			testSuccess(name);
+			return 1;
+		}
+		testFail(name);
+		return 0;
+	}
+
+	static int Test7()
+	{
+		string name = "Test7";
+		int[] customers = { };
+		int n = 3;
+		long result = HW1.QueueTime(customers, n);
+		long resultCorrect = 0;
+		if (result == resultCorrect)
+		{
+			testSuccess(name);
+			return 1;
+		}
+		Console.WriteLine($"result: {result}");
+		testFail(name);
+		return 0;
+	}
+
+	static int Test8()
+	{
+		string name = "Test8";
+		int[] customers = { 1 };
+		int n = 30;
+		long result = HW1.QueueTime(customers, n);
+		long resultCorrect = 1;
+		if (result == resultCorrect)
+		{
+			testSuccess(name);
+			return 1;
+		}
+		testFail(name);
+		return 0;
+	}
+	static int correct = 0;
+	static int wrong = 0;
+
+	static void testSuccess(string name)
+    {
+		Console.WriteLine($"{name} is completed succesfully.");
+		++correct;
+    }
+
+	static void testFail(string name)
+    {
+		Console.WriteLine($"{name} is failed.");
+		++wrong;
+	}
+
+	static void PrintResults() => Console.WriteLine("--------\n" +
+		$"Tests succesfully completed: {correct}/{correct + wrong}");
+
 }
